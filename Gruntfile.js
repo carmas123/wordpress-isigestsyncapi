db7219debe7be65484470bcb300dfe67effc6bdf
carmas123 <m.carocia83@gmail.com>
2024-10-26 23:24:11 +0200

    Modificato Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,56 +1,153 @@
-module.exports = function( grunt ) {
+module.exports = function(grunt) {
+    'use strict';
 
-	'use strict';
+    // Carica automaticamente tutti i task di Grunt
+    require('load-grunt-tasks')(grunt);
 
-	// Project configuration
-	grunt.initConfig( {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-		pkg: grunt.file.readJSON( 'package.json' ),
+        // Pulisce le directory
+        clean: {
+            build: ['build/'],
+            release: ['release/']
+        },
 
-		addtextdomain: {
-			options: {
-				textdomain: 'isigestsyncapi',
-			},
-			update_all_domains: {
-				options: {
-					updateDomains: true
-				},
-				src: [ '*.php', '**/*.php', '!\.git/**/*', '!bin/**/*', '!node_modules/**/*', '!tests/**/*' ]
-			}
-		},
+        // Copia i file nella directory build
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        src: [
+                            '**',
+                            '!node_modules/**',
+                            '!build/**',
+                            '!release/**',
+                            '!.git/**',
+                            '!.github/**',
+                            '!tests/**',
+                            '!Gruntfile.js',
+                            '!package.json',
+                            '!package-lock.json',
+                            '!phpcs.xml',
+                            '!phpunit.xml',
+                            '!composer.json',
+                            '!composer.lock',
+                            '!.gitignore',
+                            '!.editorconfig',
+                            '!.eslintrc',
+                            '!README.md',
+                            '!CHANGELOG.md'
+                        ],
+                        dest: 'build/'
+                    }
+                ]
+            }
+        },
 
-		wp_readme_to_markdown: {
-			your_target: {
-				files: {
-					'README.md': 'readme.txt'
-				}
-			},
-		},
+        // Minifica i file CSS
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'build/assets/css/admin.min.css': ['assets/css/admin.css']
+                }
+            }
+        },
 
-		makepot: {
-			target: {
-				options: {
-					domainPath: '/languages',
-					exclude: [ '\.git/*', 'bin/*', 'node_modules/*', 'tests/*' ],
-					mainFile: 'isigestsyncapi.php',
-					potFilename: 'isigestsyncapi.pot',
-					potHeaders: {
-						poedit: true,
-						'x-poedit-keywordslist': true
-					},
-					type: 'wp-plugin',
-					updateTimestamp: true
-				}
-			}
-		},
-	} );
+        // Minifica i file JavaScript
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                files: {
+                    'build/assets/js/admin.min.js': ['assets/js/admin.js']
+                }
+            }
+        },
 
-	grunt.loadNpmTasks( 'grunt-wp-i18n' );
-	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
-	grunt.registerTask( 'default', [ 'i18n','readme' ] );
-	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
-	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
+        // Crea il file .pot per le traduzioni
+        makepot: {
+            target: {
+                options: {
+                    domainPath: '/languages',
+                    exclude: ['build/.*', 'node_modules/.*'],
+                    mainFile: 'isigestsyncapi.php',
+                    potFilename: 'isigestsyncapi.pot',
+                    potHeaders: {
+                        poedit: true,
+                        'x-poedit-keywordslist': true
+                    },
+                    type: 'wp-plugin',
+                    updateTimestamp: true
+                }
+            }
+        },
 
-	grunt.util.linefeed = '\n';
+        // Controlla la qualità del codice PHP
+        phpcs: {
+            application: {
+                src: ['**/*.php', '!node_modules/**', '!build/**', '!vendor/**']
+            },
+            options: {
+                bin: 'vendor/bin/phpcs',
+                standard: 'WordPress'
+            }
+        },
 
+        // Crea il pacchetto di rilascio
+        compress: {
+            release: {
+                options: {
+                    archive: 'release/isigestsyncapi-<%= pkg.version %>.zip'
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'build/',
+                        src: ['**/*'],
+                        dest: 'isigestsyncapi/'
+                    }
+                ]
+            }
+        },
+
+        // Monitora i cambiamenti nei file
+        watch: {
+            css: {
+                files: ['assets/css/**/*.css'],
+                tasks: ['cssmin']
+            },
+            js: {
+                files: ['assets/js/**/*.js'],
+                tasks: ['uglify']
+            },
+            php: {
+                files: ['**/*.php', '!node_modules/**', '!build/**', '!vendor/**'],
+                tasks: ['phpcs']
+            }
+        }
+    });
+
+    // Task personalizzati
+    grunt.registerTask('build', [
+        'clean:build',
+        'copy:build',
+        'cssmin',
+        'uglify',
+        'makepot'
+    ]);
+
+    grunt.registerTask('package', [
+        'build',
+        'clean:release',
+        'compress:release'
+    ]);
+
+    grunt.registerTask('default', ['build']);
 };
